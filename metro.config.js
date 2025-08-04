b2b96b61fad822de6a6c8839188a76f77957baab
ballayoussouf <ballayoussouf@gmail.com>
2025-08-04 17:08:44 +0000

    delete

--- metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Ensure proper resolution for React Native
-config.resolver.platforms = ['native', 'android', 'ios', 'web'];
-
-module.exports = config;
